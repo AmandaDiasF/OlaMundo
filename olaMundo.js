7ffe44ecc63f745077d1bcf8061fcf9047c55d38
AmandaDiasF <amanda.bloom127@gmail.com>
2025-04-02 22:34:47 -0300

    Alteração testando variaveis let e cosnto

--- olaMundo.js
+++ olaMundo.js
@@ -1,1 +1,7 @@
 console.log('Hello World!!!');
+//testando tipos, const(não conseguimos atribuir outro valor do que o ja colocado inicialmente)
+//let permite a reatribuição.
+let primeiroNome = "Amanda";
+console.log("Nome: " + primeiroNome);
+primeiroNome = "Pedro";
+console.log("Nome: " + primeiroNome);
